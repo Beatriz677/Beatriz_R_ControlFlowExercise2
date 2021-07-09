775e5961502c72f14752e990bda6e1f3ac2eb1c8
Beatriz677 <86676818+Beatriz677@users.noreply.github.com>
2021-07-09 11:41:19 -0500

    Add files via upload

---
+++ collection#2.playground/Contents.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+var someInts: [Int] = []
+var collectables: [String] = ["Professor Quirrel", "Captian Marvel", "Hermione", "Ginny", "Ron", "Draco", "Hedwig", "Eleven","Malificent"]
+print(collectables)
+    collectables.sort()
+print(collectables)
+print("I have \(collectables)")
